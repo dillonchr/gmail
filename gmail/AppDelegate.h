f69ebf62c3bf376f2a348ba6f78e032207f4858b
Dillon Christensen <dillon@goloansnap.com>
2019-12-06 15:10:04 -0600

    Initial Commit

---
+++ gmail/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gmail
+//
+//  Created by Dillon Christensen on 12/6/19.
+//  Copyright © 2019 Dillon Christensen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ gmail/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  gmail
+//
+//  Created by Dillon Christensen on 12/6/19.
+//  Copyright © 2019 Dillon Christensen. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
